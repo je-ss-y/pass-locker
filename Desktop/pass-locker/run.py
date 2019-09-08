2181e86f22e912916150453e31cf886afba1ba60
je-ss-y <Nkujess@gmail.com>
2019-09-08 09:54:24 +0200

    created a contact function for the user

--- Desktop/pass-locker/run.py
+++ Desktop/pass-locker/run.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3.6
+from user import User 
+from credentials import Credentials
+
+
+def create_contact(fname,lname,number,email):
+    '''
+    Function to create a new contact
+    '''
+    new_contact = Contact(fname,lname,number,email)
+    return new_contact
